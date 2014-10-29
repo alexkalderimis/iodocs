1c7ba266fa9aefbf165eba48818e091cbeec13c1
Vivek Krishnakumar <vivekkrishnakumar@gmail.com>
2014-10-28 21:57:19 -0400

    added https protocol support; allow excluding methods requiring authn

--- app.js
+++ app.js
@@ -156,14 +156,19 @@ function initAppConfig () {
 
       logger.debug("Fetching service listing from " + serviceListingURI);
 
-      req = http.get(serviceListingURI, function responseHandler (res) {
+      var doget = (api.protocol === 'https') ? https.get : http.get;
+      req = doget(serviceListingURI, function responseHandler (res) {
           var body = "";
           res.on('data', function(chunk) { body += chunk; });
           res.on('error', handleError);
           res.on('end', function done () {
               logger.log('debug', "Retrieved service listing for %s", name);
               try {
-                imAPIs[name] = JSON.parse(body);
+                if (api.excludeRequiresAuthn === true) {
+                  imAPIs[name] = JSON.parse(body, reviver);
+                } else {
+                  imAPIs[name] = JSON.parse(body);
+                }
               } catch (e) {
                 var msg = "Error parsing service listing for " + name + ": " + e;
                 handleError(new Error(msg));
@@ -176,6 +181,15 @@ function initAppConfig () {
         delete apisConfig[name];
       }
     }
+
+    /* exclude endpoints that require authentication
+       set 'excludeRequiresAuthn': 'true' in apiconfig.json */
+    function reviver(k, v) {
+      if (k === 'methods' && v[0]['RequiresAuthentication'] === 'true') {
+          return undefined;
+      }
+      return v;
+    }
   });
 }
 
@@ -185,7 +199,7 @@ initAppConfig();
 // Refresh config every configured interval, or every hour.
 setInterval(initAppConfig, (config.refreshInterval || 60 * 60) * 1000);
 
-var app = module.exports = express(); 
+var app = module.exports = express();
 
 app.configure(function() {
 
@@ -340,7 +354,7 @@ function processRequest(req, res, next) {
                     logger.debug(apiSecret);
                     logger.debug(accessToken);
                     logger.debug(accessTokenSecret);
-                    
+
                     var oa = new OAuth(apiConfig.oauth.requestURL || null,
                                        apiConfig.oauth.accessURL || null,
                                        apiKey || null,
@@ -479,7 +493,7 @@ function processRequest(req, res, next) {
 
         // Add credentials, if provided.
         if (credentials) {
-          apiKey = credentials.token; 
+          apiKey = credentials.token;
           if (!reqQuery.headerNames) reqQuery.headerNames = [];
           if (!reqQuery.headerValues) reqQuery.headerValues = [];
 
